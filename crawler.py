595cc7b038c0a01d98dcebd1d1e266217c8cde67
showbilia <frunnever@gmail.com>
2024-04-27 19:30:30 +0800

    fix:Extract the authors, publication platform, and year from the citation text.

--- crawler.py
+++ crawler.py
@@ -4,7 +4,7 @@ from bs4 import BeautifulSoup
 from selenium import webdriver
 from urllib.parse import quote_plus
 from details import extract_citation_text
-from click import access_article, random_sleep, find_next_page_button, close_citation_modal
+from click import access_article, random_sleep, find_next_page_button, close_citation_modal, check_captcha, attempt_citation_click
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
@@ -26,20 +26,23 @@ def parse_articles(driver, results, article_count):
     """解析文章数据并保存"""
     html = driver.page_source
     soup = BeautifulSoup(html, 'html.parser')
-    articles = soup.select('#gs_res_ccl_mid .gs_ri')
+    articles = soup.select('#gs_res_ccl_mid .gs_r.gs_or.gs_scl')
 
 
-    for article in articles:
+    for index, article in enumerate(articles, start=1):
+        article = article.select_one('.gs_ri')
         title = article.select_one('.gs_rt').text
         
-        # Wait for any potential overlays to disappear
-        wait = WebDriverWait(driver, 10)
-        cite_button = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, 'a.gs_or_cit.gs_or_btn.gs_nph')))
+        article_element = driver.find_elements(By.CSS_SELECTOR, '#gs_res_ccl_mid .gs_ri')[index - 1]
+        cite_button = article_element.find_element(By.CSS_SELECTOR, 'a.gs_or_cit.gs_or_btn.gs_nph')
         
         # Execute JavaScript click
-        driver.execute_script("arguments[0].click();", cite_button)
+        driver.execute_script("arguments[0].scrollIntoView();", cite_button)
+        cite_button.click()
 
-        # Extract citation in GB/T 7714 format
+        attempt_citation_click(driver, cite_button)
+
+        # Extract citation in MLA format
         try:
             # Ensure the citation modal is fully loaded
             citation_text, authors, platform, year = extract_citation_text(driver)
@@ -47,7 +50,7 @@ def parse_articles(driver, results, article_count):
             print(f"Failed to extract citation: {e}")
             citation_text = "Citation extraction failed."
 
-        random_sleep(0.5,1.5)
+        random_sleep(0.5,1)
         # Close the citation modal if needed
         close_citation_modal(driver)
 
@@ -65,25 +68,10 @@ def parse_articles(driver, results, article_count):
     next_page_button = find_next_page_button(driver)
     if next_page_button:
         next_page_button.click()
-        random_sleep(2, 5)  # 随机延时，模仿用户行为
+        random_sleep(1.5, 3)  # 随机延时，模仿用户行为
         parse_articles(driver, results, article_count)
     else:
         print("Reached the last page or no next page button found.")
-
-def check_captcha(driver):
-    """Check for the presence of a CAPTCHA and wait for the user to complete it."""
-    try:
-        # Check for the presence of a CAPTCHA element on the page
-        captcha_element = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, 'gs_captcha_f')))
-
-        if captcha_element:
-            print("CAPTCHA detected. Please complete the CAPTCHA within 10 minutes.")
-            # Wait for up to 10 minutes for the CAPTCHA to be completed
-            WebDriverWait(driver, 600).until(EC.staleness_of(captcha_element))
-            print("CAPTCHA completed.")
-    except TimeoutException:
-        print("CAPTCHA was not completed within 10 minutes.")
-
 
 def main():
     if len(sys.argv) < 2:
@@ -95,9 +83,14 @@ def main():
     query = quote_plus(article_name)
     # 构建搜索URL
     article_url = f"https://scholar.google.com/scholar?hl=zh-CN&as_sdt=0%2C5&q={query}"    
+    driver.get(article_url)
+    # 检查是否有人机验证
+    # check_captcha(driver)
+    # check_captcha(driver)
+
     access_article(driver, article_url)
     # 检查是否有人机验证
-    check_captcha(driver)
+    # check_captcha(driver)
     # maximze the window
     driver.maximize_window()
     # 解析文章数据
